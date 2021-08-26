21445c39074cbdf01f461af847c1d1010db807f0
Samuel <Samuelpmontague@gmail.com>
2021-08-25 22:15:56 -0500

    refactored Detail page to redux

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
+import { connect } from 'react-redux';
 
 import Cart from '../components/Cart';
-import { useStoreContext } from '../utils/GlobalState';
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
@@ -14,8 +14,7 @@ import { QUERY_PRODUCTS } from '../utils/queries';
 import { idbPromise } from '../utils/helpers';
 import spinner from '../assets/spinner.gif';
 
-function Detail() {
-  const [state, dispatch] = useStoreContext();
+function Detail(props) {
   const { id } = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({});
@@ -114,4 +113,7 @@ function Detail() {
   );
 }
 
-export default Detail;
+const mapStateToProps =(state) => {
+  return {...state.products};
+}
+export default connect(mapStateToProps)(Detail);
